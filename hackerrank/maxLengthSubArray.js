c6937d4a85771da1d52a5e06b502804c92111729
PoppinMic <cjma1990au@gmail.com>
2019-01-24 23:28:06 +1100

    sliding window

---
+++ hackerrank/maxLengthSubArray.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const maxLength = (arr, k) => {
+  let count = 0,
+    maxCount = 0,
+    sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (sum + arr[i] <= k) {
+      sum += arr[i];
+      count++;
+    } else if (sum !== 0) {
+      sum = sum - arr[i - count] + arr[i];
+    }
+    maxCount = Math.max(maxCount, count);
+  }
+  return maxCount;
+};
+
+const testArray = [5, 3, 4, 1, 2, 6];
+const testK = 12;
+
+// console.log(maxLength(testArray, testK));
